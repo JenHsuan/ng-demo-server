a4d6e735b6815322bd0a4f6416dbc7b65faa58e4
JenHsuan <ofalpha@gmail.com>
2022-01-16 16:40:35 +0800

    fix bugs

--- server.js
+++ server.js
@@ -8,28 +8,28 @@ const path = require('path');
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-// Fake data
-const products = [{
-  sku: 'MYSHOES',
-  name: 'Black Running Shoes',
-  imageUrl: hostname + '/assets/images/products/black-shoes.jpg',
-  department: ['Men', 'Shoes', 'Running Shoes'],
-  price: 109.99
-}, {
-  sku: 'NEATOJACKET',
-  name: 'Blue Jacket',
-  imageUrl: hostname + '/assets/images/products/blue-jacket.jpg',
-  department: ['Women', 'Apparel', 'Jackets & Vests'],
-  price: 238.99
-}, {
-  sku: 'NICEHAT',
-  name: 'A Nice Black Hat',
-  imageUrl: hostname + '/assets/images/products/black-hat.jpg',
-  department: ['Men', 'Accessories', 'Hats'],
-  price: 29.99
-}];
-
 app.get('/api/products', (req, res) => {
+  const prefix = req.protocol + '://' + req.get('host');
+  // Fake data
+  const products = [{
+    sku: 'MYSHOES',
+    name: 'Black Running Shoes',
+    imageUrl: prefix + '/assets/images/products/black-shoes.jpg',
+    department: ['Men', 'Shoes', 'Running Shoes'],
+    price: 109.99
+  }, {
+    sku: 'NEATOJACKET',
+    name: 'Blue Jacket',
+    imageUrl: prefix +  '/assets/images/products/blue-jacket.jpg',
+    department: ['Women', 'Apparel', 'Jackets & Vests'],
+    price: 238.99
+  }, {
+    sku: 'NICEHAT',
+    name: 'A Nice Black Hat',
+    imageUrl: prefix +  '/assets/images/products/black-hat.jpg',
+    department: ['Men', 'Accessories', 'Hats'],
+    price: 29.99
+  }];
   res.json(products);
 });
 
